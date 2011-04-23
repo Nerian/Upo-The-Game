9a9495d3c15e901f091e0f2f7d5acbf21e84800e
Nerian <siotopo@gmail.com>
2011-04-24 01:50:00 +0200

    readme

---
+++ README.mk
@@ -0,0 +1,17 @@
+### UPO The Game
+
+This is a 2D space shooter where you pilot a Mac OS X powered super deadly spaceship.
+
+Your mission, quite simple. Kill all those ugly aliens.
+
+### Background
+
+I wanted to do a video game as my final Project for my degree. Normally you do your project at the last year, but I took that course at my second year because I just couldn't wait more.
+
+It was an awesome experience and I learned a lot. My tutor was Prof. Domingo Savio Rodríguez Baena. http://www.upo.es/eps/rodriguez. I remember his first words 'I am not here to tell you how to program this video game, I don't even know what Pygame is or how does Python syntax looks like. I am here just to give you advise about software development. You are on your on'.                                                             
+
+We went on in an iterative process. Each week we will sit down and ponder what we can do in a couple of days. For example, make the ships collide. And I will do whatever it takes to make that happen. No formal documentation to write, no sense-less state diagrams or any other bullshit. Do you have, at the end of the week, what we agreed? no? what happen?. Yes? that's awesome.
+
+I think that this is the best thing any professor can do. Just let your student discover what he can do. Let curiosity pave the path to its own success.
+
+As a result of this freedom, I taught myself Python, Pygame and Mercurial. 
